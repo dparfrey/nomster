a0ed091d83606cb993568a3b9c564a587aab907e
Dave Parfrey <dparfrey@gmail.com>
2019-05-23 11:57:04 -0400

    28

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true, length: { minimum: 3 }
 end
