a6e5d078bdec21b58388d41ea80c7fea0a8e0f3e
Dave Parfrey <dparfrey@gmail.com>
2019-05-24 08:43:00 -0400

    41

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,18 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost: 3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mailgun.org',
+    port: 587,
+    domain: 'gendereqa.com',
+    user_name: ENV['MAILGUN_EMAIL'],
+    password: ENV['MAILGUN_PSWD'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
